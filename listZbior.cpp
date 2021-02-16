247a8006b00d643238d8512de55b11619dea543d
Mariusz Perzynski <m.perzyk@interia.pl>
2021-02-16 14:44:25 +0100

    check list with add with insert to value

--- listZbior.cpp
+++ listZbior.cpp
@@ -16,7 +16,19 @@
 
 using namespace std;
 
+
+void funct1(){
+    // dodanie za ostatnym elementem, wartosci 100......
+    list<int> giLit = {1,2,3,4,4,3,2,1};
+    giLit.insert(--giLit.end(), 100);
+    printSTL(giLit , " " , true );
+    //-------------------------------------------
+}
+
+
+
 int main(){
+    funct1();
 
 return 0;
 }
