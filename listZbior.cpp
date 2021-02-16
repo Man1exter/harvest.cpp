586b385aa8d568f6dfb83fd25ca54982d0a8aa20
Mariusz Perzynski <m.perzyk@interia.pl>
2021-02-16 14:39:17 +0100

    include 2 more lib/s

---
+++ listZbior.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <array>
+#include <vector>
+#include <map>
+#include <set>
+#include <list>
+#include <forward_list>
+#include <typeinfo>
+#include <string>
+#include <ctime>
+#include <algorithm>
+#include <deque>
+#include <functional>
+#include <typeinfo>
+#include "numbers.h"
+
+using namespace std;
+
+int main(){
+
+return 0;
+}
